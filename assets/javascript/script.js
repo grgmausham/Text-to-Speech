d4cc8b008dc3be2a2e328764fe1ac16cbc556054
Mausham Gurung <maushamgurung999@Maushams-Air.lan>
2024-04-19 17:27:07 +0100

    adding a function to add voices from dropdown menu

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,12 +5,18 @@ let voiceSelect = document.querySelector("select");
 window.speechSynthesis.onvoiceschanged = () => {
     //provides all the default voices available in the device
     voices = window.speechSynthesis.getVoices();
- 
-}
+    speech.voice = voices[0];
 
+    voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)));
+ 
+};
+//select and apply voices from dropdown menu
+voiceSelect.addEventListener("change", () =>{
+    speech.voice = voices[voiceSelect.value]
+})
 
 // converts textarea text into default system speeech
 document.querySelector("button").addEventListener("click", () => {
     speech.text = document.querySelector("textarea").value;
     window.speechSynthesis.speek(speech);
-})
+});
